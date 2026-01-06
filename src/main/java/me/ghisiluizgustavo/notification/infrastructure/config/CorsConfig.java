abf0630e161751819b44c68f74f5bf7d49a9a0b5
Luiz Gustavo Ghisi Flores <ghisiluizgustavo@gmail.com>
2026-01-06 14:49:42 -0300

    chore: enable cors

---
+++ src/main/java/me/ghisiluizgustavo/notification/infrastructure/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package me.ghisiluizgustavo.notification.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setExposedHeaders(List.of("*"));
+        config.setMaxAge(3600L);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", config);
+        
+        return new CorsFilter(source);
+    }
+}
